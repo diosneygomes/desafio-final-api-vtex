92dfdfe96251f1ca9e13a0501bd0e9f8224d41a2
Diósney <diosneyf.gomes@gmail.com>
2021-08-27 23:06:10 -0300

    Configuracao do ambiente

---
+++ node/index.ts
@@ -0,0 +1,30 @@
+import type { ClientsConfig, ServiceContext } from '@vtex/api'
+import { method, Service } from '@vtex/api'
+
+import { Clients } from './clients'
+import { status } from './middlewares/status'
+
+const TIMEOUT_MS = 800
+
+const clients: ClientsConfig<Clients> = {
+  implementation: Clients,
+  options: {
+    default: {
+      retries: 2,
+      timeout: TIMEOUT_MS,
+    },
+  },
+}
+
+declare global {
+  type Context = ServiceContext<Clients>
+}
+
+export default new Service({
+  clients,
+  routes: {
+    status: method({
+      GET: [status],
+    }),
+  },
+})

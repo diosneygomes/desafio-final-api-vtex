f0947a4bfc550ca0376dbc91821341a0eaac0684
rtof83 <85233663+rtof83@users.noreply.github.com>
2021-08-29 11:13:06 -0300

    Add files via upload
    post

--- node/index.ts
+++ node/index.ts
@@ -3,12 +3,16 @@ import { method, Service } from '@vtex/api'
 
 import { Clients } from './clients'
 import { status } from './middlewares/status'
+import { statusPost } from './middlewares/statusPost'
 
 const TIMEOUT_MS = 800
 
+// This is the configuration for clients available in `ctx.clients`.
 const clients: ClientsConfig<Clients> = {
+  // We pass our custom implementation of the clients bag, containing the Status client.
   implementation: Clients,
   options: {
+    // All IO Clients will be initialized with these options, unless otherwise specified.
     default: {
       retries: 2,
       timeout: TIMEOUT_MS,
@@ -17,14 +21,20 @@ const clients: ClientsConfig<Clients> = {
 }
 
 declare global {
+  // We declare a global Context type just to avoid re-writing ServiceContext<Clients, State> in every handler and resolver
   type Context = ServiceContext<Clients>
 }
 
+// Export a service that defines route handlers and client options.
 export default new Service({
   clients,
   routes: {
+    // `status` is the route ID from service.json. It maps to an array of middlewares (or a single handler).
     status: method({
       GET: [status],
     }),
+    statusOnly: method({
+      POST: [statusPost]
+    })
   },
 })

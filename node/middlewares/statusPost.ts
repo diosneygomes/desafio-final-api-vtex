f4dcc6538f61a40078edf2b0cd740942bfac1566
Diósney <diosneyf.gomes@gmail.com>
2021-08-29 14:35:08 -0300

    configura requisiçao post

--- node/middlewares/statusPost.ts
+++ node/middlewares/statusPost.ts
@@ -1,22 +1,22 @@
-import { json } from 'co-body'
-import { UserInputError } from '@vtex/api'
+ import { json } from 'co-body'
+// import { UserInputError } from '@vtex/api'
 
 export async function statusPost(ctx: Context, next: () => Promise<any>) {
-  const body = await (json(ctx.req)) as { code: number }
+ const body = await (json(ctx.req))
+console.log(body);
+  // if (!body?.code) {
+  //   throw new UserInputError('Please suply the code')
+  // }
 
-  if (!body?.code) {
-    throw new UserInputError('Please suply the code')
-  }
+  // const { code } = body
 
-  const { code } = body
-
-  const res = await ctx.clients.status.getStatus(code).catch((reason) => {
-    return reason?.response?.data
-  })
+  // const res = await ctx.clients.status.getStatus(code).catch((reason) => {
+  //   return reason?.response?.data
+  // })
 
   ctx.set('Cache-Control', 'no-cache no-store')
-  ctx.body = res
-  ctx.status = code
+  ctx.body = "ok"
+  ctx.status = 200
 
   await next()
 }
